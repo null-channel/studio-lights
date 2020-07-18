753aff21f1e16e12c4341df3ae7a0b36706c3274
Marek Counts <countsmarek@gmail.com>
2020-07-18 13:15:11 -0400

    updated arduino project for multi-lights

--- arduino/studio-lights.ino
+++ arduino/studio-lights.ino
@@ -5,27 +5,32 @@
   #include <avr/power.h>
 #endif
 #include <SPI.h>
-//#include <WiFi.h>
-//#include <WiFi101.h>
 
 #include <WiFiNINA.h>
 #include <ArduinoHttpClient.h>
 
-//Defines - used as constants, not good practice in c++
-#define PIN_1 6
+/* 
+ *  Defines - used as constants, not good practice in c++.
+ *  expanded when compiled  
+ *  can be cool, but don't over use.
+ */
+#define PIN_1 5
 #define PIN_2 2
 #define PIN_3 3
 #define PIN_4 4
 
-#define RELAY_PIN_1 = 8
-#define RELAY_PIN_2 = 9
-
+//BANK_SIZE is the number of lights in a bank of lights, used to fill
 #define BANK_SIZE 16
+//How often to check for updates in miliseconds
 #define DELAY 5000
+
+struct light {
+  int number, red, green, blue;
+} l0,l1,l2,l3 ;
 
 //Global variables - again not good practice in other "real" programming
 char ssid[] = "ssid";     //  your network SSID (name)
-char pass[] = "yourpass";  // your network password
+char pass[] = "wifipass";  // your network password
 int status = WL_IDLE_STATUS;     // the Wifi radio's status
 char serverAddress[] = "10.1.1.50";  // server address
 int port = 32001;
@@ -54,6 +59,12 @@ void setup() {
   }
   connectToWifi();
   Serial.println("connected");
+}
+
+void collorize(Adafruit_NeoPixel pixel, uint32_t color, int bank_size) {
+  pixel.clear();
+  pixel.fill(color,0,bank_size);
+  pixel.show();
 }
 
 void connectToWifi() {
@@ -86,28 +97,40 @@ void loop() {
     // Get the request response payload
     // TODO: Parsing
 
-    const size_t capacity = JSON_OBJECT_SIZE(1) + JSON_OBJECT_SIZE(4) + 30;
+    const size_t capacity = JSON_ARRAY_SIZE(4) + 5*JSON_OBJECT_SIZE(1) + 4*JSON_OBJECT_SIZE(4) + 140;
     DynamicJsonDocument doc(capacity);
-
-    const char* json = "{\"Status\":{\"On\":true},\"Red\":0,\"Blue\":50,\"Green\":50}";
-
+    
+    const char* json = "{\"lights\":[{\"light\":{\"Number\":1,\"Red\":60,\"Blue\":0,\"Green\":38}},{\"light\":{\"Number\":2,\"Red\":60,\"Blue\":0,\"Green\":38}},{\"light\":{\"Number\":3,\"Red\":60,\"Blue\":0,\"Green\":38}},{\"light\":{\"Number\":0,\"Red\":60,\"Blue\":0,\"Green\":38}}]}";
+    
     deserializeJson(doc, client.responseBody());
-
-    is_on = doc["Status"]["On"]; // true
-
-    red = doc["Red"]; // 0
-    blue = doc["Blue"]; // 50
-    green = doc["Green"]; // 50
-    Serial.println(red);
+    
+    JsonArray lights = doc["lights"];
+    
+    JsonObject lights_0_light = lights[0]["light"];
+    l0.number = lights_0_light["Number"];
+    l0.blue = lights_0_light["Blue"];
+    l0.green = lights_0_light["Green"];
+    l0.red = lights_0_light["Red"];
+    
+    JsonObject lights_1_light = lights[1]["light"];
+    l1.number = lights_1_light["Number"];
+    l1.blue = lights_1_light["Blue"];
+    l1.green = lights_1_light["Green"];
+    l1.red = lights_1_light["Red"];
+    
+    JsonObject lights_2_light = lights[2]["light"];
+    l2.number = lights_2_light["Number"];
+    l2.blue = lights_2_light["Blue"];
+    l2.green = lights_2_light["Green"];
+    l2.red = lights_2_light["Red"];
+    
+    JsonObject lights_3_light = lights[3]["light"];
+    l3.number = lights_3_light["Number"];
+    l3.blue = lights_3_light["Blue"];
+    l3.green = lights_3_light["Green"];
+    l3.red = lights_3_light["Red"];
   }
 
-  Serial.println(red);
-
-  //right now we will turn both on, or none. could split this out later
-  if (is_on) {
-    digitalWrite(RELAY_PIN_1,HIGH);
-    digitalWrite(RELAY_PIN_2,HIGH);
-  }
 
   //Manage the light color right now all banks have the same color, could change this in the future
   //first clear them;
@@ -115,20 +138,15 @@ void loop() {
   pixels_2.clear();
   pixels_3.clear();
   pixels_4.clear();
-  uint32_t magenta = pixels_1.Color(red, blue, green);
+  uint32_t color = pixels_1.Color(l0.red, l0.blue, l0.green);
+  collorize(pixels_1, color, BANK_SIZE); 
+  color = pixels_1.Color(l1.red, l1.blue, l1.green);
+  collorize(pixels_2, color, BANK_SIZE); 
+  color = pixels_1.Color(l2.red, l2.blue, l2.green);
+  collorize(pixels_3, color, BANK_SIZE); 
+  color = pixels_1.Color(l3.red, l3.blue, l3.green);
+  collorize(pixels_4, color, BANK_SIZE); 
 
-  //fill them with the correct collor
-  pixels_1.fill(magenta,0,BANK_SIZE);
-  pixels_2.fill(magenta,0,BANK_SIZE);
-  pixels_3.fill(magenta,0,BANK_SIZE);
-  pixels_4.fill(magenta,0,BANK_SIZE);
-
-  //must call show to acctually update them
-  pixels_1.show();
-  pixels_2.show();
-  pixels_3.show();
-  pixels_4.show();
-
-  Serial.println("sleeping");
+  Serial.println("wait a second to call the api again");
   delay(DELAY);
 }
